724007f4257c4729ee9b143c82eaddcc7bd137ad
aftimxs <mario.aftimos@cetys.edu.mx>
2023-10-17 23:33:23 -0700

    jwt

--- backend/settings.py
+++ backend/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'corsheaders',
+    'rest_framework_simplejwt',
     'rest_framework_simplejwt.token_blacklist',
     'api',
 ]
