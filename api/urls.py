95ab84722d73cccca3fc95855d1976dfb91cd37d
aftimxs <mario.aftimos@cetys.edu.mx>
2023-10-27 11:10:02 -0700

    test replacing piv

--- api/urls.py
+++ api/urls.py
@@ -19,7 +19,7 @@ router.register(r'machine', views.MachineView, 'machine')
 router.register(r'product', views.ProductView, 'product')
 router.register(r'order', views.OrderView, 'order')
 router.register(r'operator', views.OperatorView, 'operator')
-router.register(r'product-info', views.ProductionInfoView, 'product-info')
+# router.register(r'product-info', views.ProductionInfoView, 'product-info')
 router.register(r'scrap', views.ScrapView, 'scrap')
 router.register(r'downtime', views.DowntimeView, 'downtime')
 router.register(r'speedloss', views.SpeedlossView, 'speedloss')
@@ -32,5 +32,5 @@ urlpatterns = [
     path('token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('register/', RegisterView.as_view(), name='register'),
-    path('test/', TestView.as_view(), name='test')
+    path('product-info/', TestView.as_view(), name='product-info')
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
